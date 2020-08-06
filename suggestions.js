adc93f9cbe180f5f44ee98804d98765f3fb5a4e8
shubhro <bshubhra@gmail.com>
2020-08-06 17:19:40 +0530

    Update suggestions.js

--- suggestions.js
+++ suggestions.js
@@ -8,3 +8,18 @@ st.addEventListener('click',function(){
 	st.style.display="none";
 	st.style.visibility="hidden";
 	ba.play();})
+	
+//Placing background video:
+var vh = window.innerHeight;
+var vw = window.innerWidth;
+var sr = vw/vh;
+var bv = document.getElementById("bgVideo");
+
+if(sr<1.78){
+	bv.style.height="100vh";
+	bv.style.left="-50%";
+	}
+else{
+	bv.style.width="100vw";
+	bv.style.left="0%";
+	}

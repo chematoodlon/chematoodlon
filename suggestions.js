efd0eba29898c9dd1ff3d13b040d66cb24f9e671
shubhro <bshubhra@gmail.com>
2020-08-06 13:48:21 +0530

    Create suggestions.js

---
+++ suggestions.js
@@ -0,0 +1,10 @@
+//Autoplaying audio:
+var ba = document.getElementById("bgAudio");
+var st = document.getElementById("soundTrigger");
+ba.style.display="none";
+ba.style.visibility="hidden";
+st.addEventListener('click',function(){
+	st.style.display="none";
+	st.style.visibility="hidden";
+	ba.play();})
+
